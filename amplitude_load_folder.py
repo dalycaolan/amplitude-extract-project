b835ba3208b3662ab5d001ade6ca743796672a76
dalycaolan <dalyk3@tcd.ie>
2026-01-12 18:36:29 +0000

    Fixed filename issue

--- amplitude_load_folder.py
+++ amplitude_load_folder.py
@@ -5,6 +5,7 @@ import os
 import boto3
 from dotenv import load_dotenv
 from modules.extract_json import unzip_and_store
+from pathlib import Path
 
 # Load .env file
 load_dotenv()
@@ -21,6 +22,7 @@ s3_client = boto3.client(
     aws_secret_access_key=aws_secret_key
 )
 
+
 # Extract the main zip file
 # zip_path = "data.zip"
 output_folder = "json_data"
@@ -31,10 +33,10 @@ for root, dirs, filenames in os.walk(output_folder):
     for filename in filenames:
         full_path=os.path.join(root,filename)
         files_to_upload.append(full_path)
-print(files_to_upload)
 
 for file in files_to_upload:
-    aws_file_destination = "python-import/" + file
+    file_path=Path(file)
+    aws_file_destination = "python-import/" + file_path.name
     output_path = file
     s3_client.upload_file(output_path, aws_bucket_name, aws_file_destination)
-    print(f"✓ Uploaded: {file}")
+    print(f"✓ Uploaded: {file}, name is {aws_file_destination}")
