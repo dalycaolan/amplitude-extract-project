900b0dc0e689fc33f93748df269a9ac54f392b0b
dalycaolan <dalyk3@tcd.ie>
2026-01-13 09:50:03 +0000

    Fixing extract function

--- extract-amplitude-data.py
+++ extract-amplitude-data.py
@@ -47,7 +47,7 @@ api_key=os.getenv('AMP_API_KEY')
 secret_key= os.getenv('AMP_SECRET_KEY')
 amp_region=os.getenv('AMP_DATA_REGION')
 
-# Read .env file
+# # Read .env file
 aws_access_key=os.getenv('AWS_ACCESS_KEY')
 aws_secret_key=os.getenv('AWS_SECRET_KEY')
 aws_bucket_name=os.getenv('BUCKET_NAME')
@@ -84,73 +84,72 @@ else:
 
 print(list_of_jsons)
 
-# boto3.list_s3_objects(aws_bucket_name)
 
-# # API endpoint is the EU residency server
-# url = 'https://analytics.eu.amplitude.com/api/2/export'
-# params = {
-#     'start': start,
-#     'end': end
-# }
+# API endpoint is the EU residency server
+url = 'https://analytics.eu.amplitude.com/api/2/export'
+params = {
+    'start': start,
+    'end': end
+}
 
-# logger.info('Parameters defined')
+logger.info('Parameters defined')
 
 
-# number_tries=0
+number_tries=0
 
-# while number_tries<3:
+while number_tries<3:
 
-#     # Make the GET request with basic authentication
-#     response = requests.get(url, params=params, auth=(api_key, secret_key))
+    # Make the GET request with basic authentication
+    response = requests.get(url, params=params, auth=(api_key, secret_key))
 
-# #Error handling
-#     response_code=response.status_code
-#     destination_pf = f'data/'
+#Error handling
+    response_code=response.status_code
+    destination_pf = f'data/'
 
-#     file_date = datetime.now().strftime('%Y%m%dT%H-%M-%S')
-#     destination_filepath = f"{destination_pf}amplitude_data_{file_date}.zip"
-#     json_filepath = f'json_data/{file_date}'
+    file_date = datetime.now().strftime('%Y%m%dT%H-%M-%S')
+    destination_filepath = f"{destination_pf}amplitude_data_{file_date}.zip"
+    json_filepath = f'json_data/{file_date}'
 
-#     logger.info('Attempting API request...')
+    logger.info('Attempting API request...')
 
-#     if response_code == 200:
+    if response_code == 200:
 
-#         # The request was successful
+        # The request was successful
 
-#         data = response.content 
-#         logger.info('Data retrieved successfully. Attempting to zip.')
-#         # JSON data files saved to a zip folder 'data.zip'
-#         with open(destination_filepath, 'wb') as file:
-#             file.write(data)
+        data = response.content 
+        logger.info('Data retrieved successfully. Attempting to zip.')
+        # JSON data files saved to a zip folder 'data.zip'
+        with open(destination_filepath, 'wb') as file:
+            file.write(data)
 
-#             print(f'Download successful lets goooo ☘️')
-#             logger.info(f'Download successful, storing as amplitude_data_{file_date}.zip') 
+            print(f'Download successful lets goooo ☘️')
+            logger.info(f'Download successful, storing as amplitude_data_{file_date}.zip') 
         
-#             try:
-#                 unzip_and_store(destination_filepath, file_date)
-#                 print('Storage successfull lets gooo ☘️ ☘️')
-#                 logger.info(f'Unzipping and storage successful, loaded into {json_filepath}')
-#             except:
-#                 with Exception as e:
-#                     logger.warning(f'Unzipping unsuccessful :(')
+            try:
+                unzip_and_store(destination_filepath, file_date)
+                print('Storage successfull lets gooo ☘️ ☘️')
+                logger.info(f'Unzipping and storage successful, loaded into {json_filepath}')
+            except:
+                with Exception as e:
+                    logger.warning(f'Unzipping unsuccessful :(')
     
-#         break
+        break
 
-#     elif response_code<200 and response_code>499:
+    elif response_code<200 and response_code>499:
         
-#         # Print response reason and try again
+        # Print response reason and try again
         
-#         logger.warning(response.reason) 
-#         time.sleep(10)
+        logger.warning(response.reason) 
+        time.sleep(10)
     
-#         count+=1
+        count+=1
 
 
-#     else:
+    else:
         
-#         # The request failed; print the error
-#         print(f'Error {response_code}: {response.text}')
-#         logger.error(f'Error {response_code}: {response.text}')
+        # The request failed; print the error
+        print(f'Error {response_code}: {response.text}')
+        logger.error(f'Error {response_code}: {response.text}')
 
-#         break
+        break
 
